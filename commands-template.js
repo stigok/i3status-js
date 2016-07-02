21ec6d15e0101143af5a0e2da342fd1c2a479991
stigok <stig@stigok.com>
2016-07-02 23:37:14 +0200

    Load commands from separate file

---
+++ commands-template.js
@@ -0,0 +1,48 @@
+module.exports = [
+  // Networking
+  {
+    cmd: 'ip address',
+    actions: [
+      data => {
+        let matches = [];
+        data.replace(/([\d.]{8,20})/g, (str, match) => matches.push(match));
+        matches = matches.filter(m => !m.startsWith('127'));
+        return 'IP: ' + matches[0];
+      }
+    ]
+  },
+  {
+    cmd: 'nmcli connection show --active',
+    actions: [
+      data => {
+        let matches = [];
+        data.replace(/(^\w+)(?:.+vpn)/m, (str, match) => matches.push(match));
+        if (!matches.length) {
+          return 'VPN: no';
+        }
+        return 'VPN: ' + matches[0];
+      }
+    ]
+  },
+  // Brightness
+  {
+    cmd: 'setbrightness',
+    actions: [
+      data => 'Brightness: ' + data.match(/level is (\d+)/)[1]
+    ]
+  },
+  // Battery
+  {
+    cmd: 'acpi --battery',
+    actions: [
+      data => 'Battery: ' + data.match(/(\d{1,3}%)/)[1]
+    ]
+  },
+  // Time
+  {
+    cmd: 'date +"%F %R:%S"',
+    actions: [
+      data => 'Time: ' + data.replace('\n', '')
+    ]
+  }
+];
--- index.js
+++ index.js
@@ -5,54 +5,7 @@ const exec = require('./exec-promise');
 // Update interval (ms)
 const updateInterval = process.argv[2] || 3000;
 
-const commands = [
-  // Networking
-  {
-    cmd: 'ip address',
-    actions: [
-      data => {
-        let matches = [];
-        data.replace(/([\d.]{8,20})/g, (str, match) => matches.push(match));
-        matches = matches.filter(m => !m.startsWith('127'));
-        return 'IP: ' + matches[0];
-      }
-    ]
-  },
-  {
-    cmd: 'nmcli connection show --active',
-    actions: [
-      data => {
-        let matches = [];
-        data.replace(/(^\w+)(?:.+vpn)/m, (str, match) => matches.push(match));
-        if (!matches.length) {
-          return 'VPN: no';
-        }
-        return 'VPN: ' + matches[0];
-      }
-    ]
-  },
-  // Brightness
-  {
-    cmd: 'setbrightness',
-    actions: [
-      data => 'Brightness: ' + data.match(/level is (\d+)/)[1]
-    ]
-  },
-  // Battery
-  {
-    cmd: 'acpi --battery',
-    actions: [
-      data => 'Battery: ' + data.match(/(\d{1,3}%)/)[1]
-    ]
-  },
-  // Time
-  {
-    cmd: 'date +"%F %R:%S"',
-    actions: [
-      data => 'Time: ' + data.replace('\n', '')
-    ]
-  }
-];
+const commands = require('./commands.js');
 
 // Run the status update worker
 (function worker() {
