eff63b3faf61a30024bb8625ded850569aacfa71
a <a@example.com>
2016-07-02 22:47:41 +0200

    Comments

--- exec-promise.js
+++ exec-promise.js
@@ -1,6 +1,7 @@
 const exec = require('child_process').exec;
 const Promise = require('bluebird');
 
+// Simply makes child_process.exec return a promise
 function execPromise(command, options) {
   return new Promise((resolve, reject) => {
     exec(command, options, (err, stdout, stderr) => {
