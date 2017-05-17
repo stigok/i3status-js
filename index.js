95f920784f1cd0d506ff72eb15467c2ad45c8cc7
Stig Otnes Kolstad <stig@stigok.com>
2017-05-17 13:06:27 +0200

    Read env, not argv
    Fixes #1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path')
 const cwd = path.dirname(process.argv[1])
 const mapObject = require('./utils').mapObject
 
-const pluginDir = process.argv.PLUGIN_DIR || path.join(cwd, 'commands.d/')
+const pluginDir = process.env.PLUGIN_DIR || path.join(cwd, 'commands.d/')
 const separator = ' | '
 const clearScreen = '\x1B[2J\x1B[0f'
 
